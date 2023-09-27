c16f9ebf474a0b78bfc9b9f24474340dbff60159
Kosimjon <MeKess>
2023-09-27 10:13:07 +0500

    #19 Validation of external properties (props)

--- src/functions.js
+++ src/functions.js
@@ -1,15 +1,10 @@
-import {
-  PAGE_ACTIVITIES, 
-  PAGE_PROGRESS, 
-  PAGE_TIMELINE, 
-  HOURS_IN_DAY  
-} from './constants'
+import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE, HOURS_IN_DAY } from './constants'
 
 export function normalizePageHash() {
   const hash = window.location.hash.slice(1)
 
   if ([PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE].includes(hash)) {
-    return hash  
+    return hash
   }
 
   window.location.hash = PAGE_TIMELINE
